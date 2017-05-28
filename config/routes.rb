3e725f5982acd4dd1a0dc777e5bfa74b8200bca3
Nick Block <nicholasblock78@gmail.com>
2017-05-28 09:08:06 -0500

    Something on site now. Add in capistrano into gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :sites
+  resources :entries
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	root 'sites#index'
 end
